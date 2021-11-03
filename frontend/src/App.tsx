1ff01f36331bd73be81cd38303c79b7717d855eb
cardosoandreia <deiacardoso89@gmail.com>
2021-11-03 12:54:06 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-        <div>
-        <h1 className="Text-primary">Ola Mundo!</h1>
-        </div>
-  );
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Salve Galera!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+      </>
+      );
 }
 
-export default App;
+      export default App;
