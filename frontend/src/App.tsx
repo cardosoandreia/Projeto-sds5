df82d1297ab9ad392726cdee6344c8facdaeda17
cardosoandreia <deiacardoso89@gmail.com>
2021-11-03 15:20:13 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,39 @@
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
+import BarChart from "components/Footer";
+import DonutChart from "components/BarChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Salve Galera!</h1>
-        <DataTable />
+        <h1 className="text-primary py-3">Dastboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text secondary">Taxa de sucesso(%)</h5>
+
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5>Todas Vendas</h5>
+
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+
+          <h2 className="text-primary">Todas vendas </h2>
       </div>
-      <Footer />
-      </>
-      );
+
+      <DataTable />
+      </div>
+
+    <Footer />
+    </>
+  );
 }
 
-      export default App;
+export default App;
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
-const footer = () => {
+const Footer = () => {
+
     return (
+
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="linkedin.com/in/andreia-cardoso-a97200200" target="_blank" rel="noreferrer">Andreia Cardoso</a></p>
@@ -7,8 +9,7 @@ const footer = () => {
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
-
     );
 }
 
-export default footer;
+export default Footer;
